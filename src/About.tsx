08ae0b9f8c7c4e3df7d38de09c7f30a762f7bcce
novafocks <95980374+novafocks@users.noreply.github.com>
2022-09-03 19:10:32 -0400

    Most of About page

--- src/About.tsx
+++ src/About.tsx
@@ -1,11 +1,22 @@
 import PageHeader from "./components/pageheader";
 import Footer from "./components/footer";
 
-import { createStyles, Image, SimpleGrid, Space, Text, Title } from "@mantine/core";
+import { 
+	createStyles, 
+	Image, 
+	SimpleGrid, 
+	Space, 
+	Text, 
+	Title 
+} from "@mantine/core";
 
 export default function About() {
 
 	const useStyles = createStyles(() => ({
+		txt: {
+			fontSize: "1.2rem"
+		},
+
 		statement: {
 			fontSize: "84px",
 
@@ -34,9 +45,10 @@ export default function About() {
 				<Title 
 					color="white"
 					order={1}
+					weight="bolder"
 					className={classes.statement}
 				>
-					We help local governments aid communities vulnerable to the effects of urban heat islands.
+					We help local municipalities aid communities vulnerable to the effects of urban heat islands.
 				</Title>
 			</div>
 			<div className={classes.whiteContainer}>
@@ -47,12 +59,25 @@ export default function About() {
 						{ maxWidth: 768, cols: 1, spacing: "md" }
 					]}
 				>
-					<Title order={3}>
-						What are heat islands?
-					</Title>
-					<Title order={3}>
-						Why should we care?
-					</Title>
+					<div>
+						<Title order={3}>
+							What are urban heat islands?
+						</Title>
+						<Text className={classes.txt}>
+							Cities and urban areas hold not only hundreds of thousands of people, but also buildings and structures that absorb the sun's energy and re-emit it in larger amounts than other areas. These areas then become relatively extremely hot, even during winter in many places. Because the heat is localized within these areas, they are called <strong style={{ color: "#11c269" }}>urban heat islands</strong>.
+					</Text>
+					</div>
+					<div>
+						<Title order={3}>
+							Why should we care?
+						</Title>
+						<Text className={classes.txt}>
+							As climate change worsens around the world, so too have the effects of urban heat islands on nearby cities and their populations. Summers lasting longer means heat will be more consistently impactful, local governments and individuals need to pay more for short term solutions like air conditioning, and worst of all: the rate of heat-related injuries and mortalities will only increase as time goes on.
+							<br />
+							<br />
+							This is especially true for underprivileged communities, who often live in more concentrated areas and may not be able to afford methods of combatting the heat. Seniors and young children are also especially vulnerable.
+					</Text>
+					</div>
 				</SimpleGrid>
 			</div>
 			<div className={classes.greenContainer}>
@@ -69,7 +94,12 @@ export default function About() {
 						{ maxWidth: 768, cols: 1, spacing: "md" }
 					]}
 				>
-					<Text></Text>
+					<Text 
+						className={classes.txt}
+						color="white"
+					>
+						fuck you we ain't doin shit
+					</Text>
 					<Image
 						radius="md"
 						src="https://scholar.harvard.edu/files/styles/os_files_xxlarge/public/ccwagner/files/heatvulnerabilityindex_map.png?m=1592934731&itok=M3u7RXP0"
