97b3db8e4f7d946a26632150e8a572e510cad880
thefinaljob <nikolai_peram@outlook.com>
2022-09-03 12:00:53 -0400

    working map with data, need to change the colors based off Heat Severity Index

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from "./components/footer";
 import { PageHeader } from "./components/pageheader";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import React, { useRef, useEffect, useState } from "react";
+import heat from "./data/heat_severity_vulnerability.geojson";
 
 mapboxgl.accessToken = process.env.REACT_APP_PUBLIC_API_KEY;
 
@@ -21,15 +22,49 @@ export default function App() {
 			center: [lng, lat],
 			zoom: zoom,
 		});
+
+		map.current.on('load', () => {
+			map.current.addSource('trees', {
+			  type: 'geojson',
+			  data: heat
+			});
+
+			map.current.addLayer(
+				{
+					'id': 'heat',
+					'type': 'fill',
+					'source': 'trees', // reference the data source
+					'layout': {},
+					'paint': {
+					'fill-color': '#0080ff', // blue color fill
+					'fill-opacity': 0.5
+					}
+				}
+			  );
+
+			  map.current.addLayer({
+				'id': 'outline',
+				'type': 'line',
+				'source': 'trees',
+				'layout': {},
+				'paint': {
+				'line-color': '#000',
+				'line-width': 3
+				}
+				});
+			  
+		});
+		
 	});
 
 	useEffect(() => {
 		if (!map.current) return; // Wait for map to initialize
+
 		map.current.on("move", () => {
 			setLng(map.current.getCenter().lng.toFixed(4));
 			setLat(map.current.getCenter().lat.toFixed(4));
 			setZoom(map.current.getZoom().toFixed(2));
-		});
+		});	
 	});
 
 	return (
