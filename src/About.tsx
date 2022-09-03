22093c2cb392df80b7aa8ac19bcd2cdc3b3e8ffd
novafocks <95980374+novafocks@users.noreply.github.com>
2022-09-03 15:26:59 -0400

    Template for About page

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,7 @@
 import PageHeader from "./components/pageheader";
 import Footer from "./components/footer";
 
-import { createStyles, SimpleGrid, Text, Title } from "@mantine/core";
+import { createStyles, Image, SimpleGrid, Space, Text, Title } from "@mantine/core";
 
 export default function About() {
 
@@ -39,10 +39,55 @@ export default function About() {
 					We help local governments aid communities vulnerable to the effects of urban heat islands.
 				</Title>
 			</div>
+			<div className={classes.whiteContainer}>
+				<SimpleGrid 
+					cols={2} 
+					spacing="xl"
+					breakpoints={[
+						{ maxWidth: 768, cols: 1, spacing: "md" }
+					]}
+				>
+					<Title order={3}>
+						What are heat islands?
+					</Title>
+					<Title order={3}>
+						Why should we care?
+					</Title>
+				</SimpleGrid>
+			</div>
 			<div className={classes.greenContainer}>
-				<SimpleGrid>
-					<Title>FUCK YOU</Title>
-					<Title>FUCK YOU MORe</Title>
+				<Title
+					order={2}
+					color="white"
+				>
+					What are we doing about it?
+				</Title>
+				<SimpleGrid 
+					cols={2}
+					spacing="xl"
+					breakpoints={[
+						{ maxWidth: 768, cols: 1, spacing: "md" }
+					]}
+				>
+					<Text></Text>
+					<Image
+						radius="md"
+						src="https://scholar.harvard.edu/files/styles/os_files_xxlarge/public/ccwagner/files/heatvulnerabilityindex_map.png?m=1592934731&itok=M3u7RXP0"
+					/>
+					<Space />
+					<Title 
+						order={6}
+						color="white"
+					>
+						Source: <a 
+							href="https://scholar.harvard.edu/ccwagner/heat-vulnerability-philadelphia"
+							style={{
+								color: "white"
+							}}
+						>
+							Harvard
+						</a>
+					</Title>
 				</SimpleGrid>
 			</div>
 			<Footer />
