618a738c98c857f6e52e83c62eb8be0f458eb4c1
novafocks <95980374+novafocks@users.noreply.github.com>
2022-09-03 13:39:38 -0400

    Bigger message page

--- src/About.tsx
+++ src/About.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@mantine/core";
+import { Container, Text, Title } from "@mantine/core";
 import Footer from "./components/footer";
 import { PageHeader } from "./components/pageheader";
 
@@ -6,7 +6,30 @@ export default function About() {
 	return (
 		<>
 			<PageHeader />
-			<Text>About</Text>
+			<div 
+				style={{
+					backgroundColor: "#1BEB82",
+					padding: "80px",
+					justifyContent: "center"
+				}}
+			>
+				<Title 
+					color="white"
+					order={1} 
+					weight={800} 
+					size={90}
+				>
+					We help local municipalities and governments communities vulnerable to the effects of urban heat islands.
+				</Title>
+			</div>
+			<div
+				style={{
+					display: "flex",
+					padding: "80px"
+				}}	
+			>
+				<Title order={3}>What exactly are urban heat islands, and why should we care?</Title>
+			</div>
 			<Footer />
 		</>
 	);
