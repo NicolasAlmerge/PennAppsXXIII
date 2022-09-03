af9d1ce3968bf38b67264d83fe2ecf3917252496
Nicolas Almerge <nicolas.almerge@gmail.com>
2022-09-03 04:02:18 -0400

    Added map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import Footer from "./components/footer";
 import { PageHeader } from "./components/pageheader";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import React, { useRef, useEffect, useState } from "react";
+
+mapboxgl.accessToken = process.env.REACT_APP_PUBLIC_API_KEY;
 
 export default function App() {
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(-70.9);
+	const [lat, setLat] = useState(42.35);
+	const [zoom, setZoom] = useState(9);
+
+	useEffect(() => {
+		if (map.current) return; // initialize map only once
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [lng, lat],
+			zoom: zoom,
+		});
+	});
 	return (
-		<>
-			<PageHeader />
-			<Footer />
-		</>
+		<div>
+			<div ref={mapContainer} className="map-container" />
+		</div>
 	);
 }
--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,26 +1,47 @@
+import { useState } from "react";
+
 export default function Contact() {
+	let [message, setMessage] = useState("");
 	const submitForm = (e: any) => {
+		console.log(message);
+		e.preventDefault();
 		const inputObject = Object.fromEntries(new FormData(e.target));
 		console.log(inputObject);
-		return false;
+		return true;
 	};
 
 	return (
 		<form onSubmit={submitForm}>
 			<div>
-				<input name="first-name" placeholder="First Name" required />
-				<input name="last-name" placeholder="Last Name" required />
+				<input
+					name="first-name"
+					id="first-name"
+					placeholder="First Name"
+					required
+				/>
+				<input
+					name="last-name"
+					id="last-name"
+					placeholder="Last Name"
+					required
+				/>
 			</div>
 			<br />
 			<div>
-				<input name="email" type="email" placeholder="Email" required />
+				<input
+					name="email"
+					id="email"
+					type="email"
+					placeholder="Email"
+					required
+				/>
 			</div>
 			<br />
 			<div>
 				<textarea
 					style={{ resize: "none" }}
 					required
-					className="message"
+					id="message"
 					placeholder="Message"
 				/>
 			</div>
