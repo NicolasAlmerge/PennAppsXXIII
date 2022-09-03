b0890546ee79e6609a5aab1bc55986f149073436
novafocks <95980374+novafocks@users.noreply.github.com>
2022-09-03 14:50:30 -0400

    Blank page

--- src/About.tsx
+++ src/About.tsx
@@ -1,12 +1,6 @@
-import { Image, Text, Title, SimpleGrid, Space } from "@mantine/core";
-import Footer from "./components/footer";
-import PageHeader from "./components/pageheader";
-
 export default function About() {
-	return (
+	return(
 		<>
-			<PageHeader />
-			<Footer />
 		</>
 	);
 }
